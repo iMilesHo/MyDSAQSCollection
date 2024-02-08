c40abade2585b7a31b4c42d8261313fb210dc878
YuanlaiHe <yuanlaihe@126.com>
2024-02-08 18:25:06 -0500

    updates

--- JavaSolution/src/Main.java
+++ JavaSolution/src/Main.java
@@ -1,31 +1,15 @@
-import java.lang.String;
-import java.util.*;
-class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        if(strs==null || strs.length==0){
-            return "";
-        }
-        String prefix = strs[0];
-        for(int i=1;i<strs.length;i++){
-            while(strs[i].indexOf(prefix)!=0){
-                prefix =prefix.substring(0,prefix.length()-1);
-            }
-        }
-
-        return prefix;
-    }
-}
-
-
 public class Main {
-
     public static void main(String[] args) {
-//        Solution s = new Solution();
-//        s.longestCommonPrefix(new String[]{"abc", "efg", "abcdefg"});
-//        System.out.println("Hello world!");
-
-        String a = "";
-        String b = "";
-        System.out.println(a.indexOf(b));
+        int n = 5;
+        int[][] pascal = new int[n][];
+        for (int i = 0; i < n; i++) {
+            pascal[i] = new int[i + 1]; // since java array is 0-based
+            pascal[i][0] = 1;
+            for(int j=1; j<i; j++) {
+                pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
+            }
+            pascal[i][i] = 1;
+        }
+        System.out.println(pascal);
     }
 }
--- Notes/Array.md
+++ Notes/Array.md
@@ -10,3 +10,33 @@
     - (1,1)|(2,1)|(3,1)|(1,2)|(2,2)|(3,2)|(1,3)|(2,3)|(3,3)
 
 
+## Practice
+
+### 1. Print a pascal triangle
+```Java
+// Java
+public class PascalTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+        int[][] pascal = new int[n][];
+        for (int i = 0; i < n; i++) {
+            pascal[i] = new int[i + 1]; // since java array is 0-based
+            pascal[i][0] = 1;
+            for(int j=1; j<i; j++) {
+                pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
+            }
+            pascal[i][i] = 1;
+        }
+    }
+}
+```
+
+```Python
+# Python
+def pascal_triangle(n):
+    pascal = [[1] * (i+1) for i in range(n)]
+    for i in range(n):
+        for j in range(1, i):
+            pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j]
+    return pascal
+```
