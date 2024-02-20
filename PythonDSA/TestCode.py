2e3d3d1b12c6371904a3a4175787bf2185032bad
YuanlaiHe <yuanlaihe@126.com>
2024-02-20 17:27:17 -0500

    updates

--- PythonDSA/TestCode.py
+++ PythonDSA/TestCode.py
@@ -2,31 +2,22 @@ import sys
 from typing import List
 
 class Solution:
-    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        num = 0
-        if n==0:
-            return True
-        if len(flowerbed)==1:
-            if flowerbed[0] == n:  
-                return False
-            else: 
-               return True        
-        if flowerbed[0]==0 and flowerbed[1]==0:
-          num += 1
-          flowerbed[0] = 1  
-        for i in range(1,len(flowerbed)-1):
-          if flowerbed[i-1]==0 and flowerbed[i]==0 and flowerbed[i+1]==0 :
-             flowerbed[i] = 1 
-             num += 1
-        if flowerbed[-2]==0 and flowerbed[-1] == 0:
-             num += 1 
-             flowerbed[-1] = 1      
-        if n <= num:
-            return True
-        else:
-            return False      
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        if len(nums) < 3:
+            return False
+        
+        first = float('inf')
+        second = float('inf')
+        for third in nums:
+            if third > second:
+                return True
+            if third <= first:
+                first = third
+            else:
+                second = third
+        return False     
 
 # test code
 s = Solution()
-list = [0]
-print(s.canPlaceFlowers(list, 0))
+list = [0,1,3]
+print(s.increasingTriplet(list))
