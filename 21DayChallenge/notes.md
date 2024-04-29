105bb0dc3d3e5e175fa39ed08be13e11833c97ff
Lyle <imilesho@gmail.com>
2024-04-29 19:14:48 -0400

    new

--- 21DayChallenge/notes.md
+++ 21DayChallenge/notes.md
@@ -376,5 +376,39 @@ function binarySearch(data, k) {
 ```
 
 ```python
+def binarySearch(arr: List[int], k: int) -> int:
+    left, right = 0, len(arr) - 1
 
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == k:
+            return mid
+        elif arr[mid] < k:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+```
+
+# Day 10
+
+Implement a merge sort algorithm for an array.
+
+```
+def mergeSort(arr: List[int]) -> List[int]:
+    length_arr = len(arr)
+    if length_arr <= 1:
+        return arr
+    left = mergeSort(arr[:length_arr//2])
+    right = mergeSort(arr[length_arr//2:])
+
+    left_i, right_i = 0, 0
+    for i in range(len(left)+len(right)):
+        if (left_i < len(left) and left[left_i] <= right[left_i]) or right_i >= len(right):
+            arr[i] = left[left_i]
+            left_i += 1
+        elif (right_i < len(right) and left[left_i] > right[left_i]) or left_i >= len(left):
+            arr[i] = right[right_i]
+            right_i += 1
+    return arr
 ```
--- PythonDSA/TestCode.py
+++ PythonDSA/TestCode.py
@@ -1,20 +1,25 @@
 from typing import List
 from traitlets import Int
 
-def binarySearch(arr: List[int], k: int) -> int:
-    left, right = 0, len(arr) - 1
-    
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] == k:
-            return mid
-        elif arr[mid] < k:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return -1
+def mergeSort(arr: List[int]) -> List[int]:
+    length_arr = len(arr)
+    if length_arr <= 1:
+        return arr
+    left = mergeSort(arr[:length_arr//2])
+    right = mergeSort(arr[length_arr//2:])
+
+    left_i, right_i = 0, 0
+    for i in range(len(left)+len(right)):
+        if (left_i < len(left) and left[left_i] <= right[left_i]) or right_i >= len(right):
+            arr[i] = left[left_i]
+            left_i += 1
+        elif (right_i < len(right) and left[left_i] > right[left_i]) or left_i >= len(left):
+            arr[i] = right[right_i]
+            right_i += 1
+    return arr
+
 
 # Test
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-k = 1
-print(binarySearch(arr, k))
+
+arr = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+print(mergeSort(arr))
