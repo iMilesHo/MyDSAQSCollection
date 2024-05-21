67a1b2d3046235a815828fad345a6a305f53109e
Lyle <imilesho@gmail.com>
2024-05-21 17:43:10 -0400

    new

--- PythonDSA/TestCode.py
+++ PythonDSA/TestCode.py
@@ -2,30 +2,49 @@ from typing import List, Optional
 from traitlets import Int
 from collections import Counter, defaultdict, deque
 
+# 76. Minimum Window Substring
+
+# Given two strings s and t of lengths m and n respectively, return the minimum window 
+# substring
+#  of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+
 class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
-        s = set(nums)
-        longest = 0
-        while len(s) > 0:
-            temp = s.pop()
-            length = 1
-
-            temp1 = temp
-            while temp1 + 1 in s:
-                length += 1
-                temp1 += 1
-                s.remove(temp1)
+    def isMet(self, t: str, s: str) -> bool:
+        t_c = Counter(t)
+        s_c = Counter(s)
+        for k, v in t_c.items():
+            if s_c[k] < v:
+                return False
+        return True
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t):
+            return ""
+        
+        if not self.isMet(t, s):
+            return ""
+        min_ans = s
+        def helper(s1):
+            nonlocal min_ans
+            if len(min_ans) > len(s1):
+                min_ans = s1
             
-            while temp - 1 in s:
-                length += 1
-                temp -= 1
-                s.remove(temp)
-                
-            if length > longest:
-                longest = length
-        return longest
-    
-
+            left = right = s1
+            if len(s1[1:]) >= len(t) and self.isMet(t, s1[1:]):
+                left = helper(s1[1:])
+            if len(s1[:-1]) >= len(t) and self.isMet(t, s1[:-1]):
+                right = helper(s1[:-1])
+            
+            ans = s1
+            if left and left < ans:
+                ans = left
+            if right and right < ans:
+                ans = right
+            return ans
+        helper(s)
+        return min_ans
+        
+# test
+s = "acbbaca"
+t = "aba"
 sol = Solution()
-nums = [1, 0, -1]
-print(sol.longestConsecutive(nums))
+print(sol.minWindow(s, t)) # "BANC"
