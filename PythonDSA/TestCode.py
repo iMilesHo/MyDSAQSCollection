446b0b3945ce56045c04f58a670537d8b7b741a2
Lyle <imilesho@gmail.com>
2024-06-03 14:05:18 -0400

    new

--- PythonDSA/TestCode.py
+++ PythonDSA/TestCode.py
@@ -4,50 +4,39 @@ from collections import Counter, defaultdict, deque
 
 
 
-"""
-grid = [
-        [1,2],
-        [3,4]
-    ]
-possible path:
-[1,2]
-[1,2,4]
-[1,3]
-[1,3,4]
-[2,4]
-[3,4]
-"""
-def find_increasing_sequences(grid: List[List[int]]) -> int:
-    ROWS, COLS = len(grid), len(grid[0])
-    # explore direction
-    explore_dir = [(1,0), (-1,0), (0,1), (0,-1)]
-    res = 0
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        explore_dir = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        path = set()
+        ROWS, COLS = len(board), len(board[0])
 
-    def exploreable(r,c):
-        return 0 <= r < ROWS and 0 <= c < COLS
+        def valide(r,c):
+            return 0 <= r < ROWS and 0 <= c < COLS
 
-    def dfs(r, c, psn): # 1, 1, 3
-        nonlocal res
-        if psn >= 2: 
-            res += 1 # 2
-        for ar, ac in explore_dir:
-            nr, nc = r+ar,c+ac # 
-            if exploreable(nr, nc) and grid[nr][nc] > grid[r][c]:
-                dfs(nr,nc,psn+1) # 1, 1, 3
-        return
-    for i in range(ROWS):
-        for j in range(COLS):
-            dfs(i, j, 1)
-    return res
+        def dfs(r, c, idx):
+            if idx == len(word) - 1:
+                return True
+            
+            flag = False
+            for (ar, ac) in explore_dir:
+                nr, nc = r + ar, c + ac
+                print(path)
+                if valide(nr,nc) and (nr,nc) not in path and board[nr][nc] == word[idx+1]:
+                    path.add((nr,nc))
+                    flag = dfs(nr,nc,idx+1)
+                    path.remove((nr,nc))
+            
+            return flag
+        
+        for i in range(ROWS):
+            for j in range(COLS):
+                if board[i][j] == word[0]:
+                    path.add((i,j))
+                    if dfs(i,j,0):
+                        return True
+                    path.remove((i,j))
+        return False
 
 
 
-# Define the grid
-grid = [
-    [1, 2],
-    [3, 4]
-]
-
-# Find all increasing sequences
-print("Found sequences:", find_increasing_sequences(grid))
 
