06e33b9588e5ce7503fdeceeec6b51ce9b0e47a0
Lyle <imilesho@gmail.com>
2024-07-07 23:41:18 -0400

    n

--- PythonDSA/TestCode.py
+++ PythonDSA/TestCode.py
@@ -1,42 +1,28 @@
-from typing import List, Optional
-from traitlets import Int
-from collections import Counter, defaultdict, deque
-
-
-
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        explore_dir = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        path = set()
-        ROWS, COLS = len(board), len(board[0])
-
-        def valide(r,c):
-            return 0 <= r < ROWS and 0 <= c < COLS
-
-        def dfs(r, c, idx):
-            if idx == len(word) - 1:
-                return True
+    def longestConsecutive(self, nums: List[int]) -> int:
+        value_dict = {}
+        for i in nums:
+            value_dict[i] = False
+        longest_ans = 0
+        for i in nums:
+            if value_dict[i]:
+                continue
             
-            flag = False
-            for (ar, ac) in explore_dir:
-                nr, nc = r + ar, c + ac
-                print(path)
-                if valide(nr,nc) and (nr,nc) not in path and board[nr][nc] == word[idx+1]:
-                    path.add((nr,nc))
-                    flag = dfs(nr,nc,idx+1)
-                    path.remove((nr,nc))
+            temp_length = 1
+            j = i + 1
+            while value_dict.get(j) is not None:
+                temp_length += 1
+                value_dict[j] = True
+                j += 1
+                
             
-            return flag
-        
-        for i in range(ROWS):
-            for j in range(COLS):
-                if board[i][j] == word[0]:
-                    path.add((i,j))
-                    if dfs(i,j,0):
-                        return True
-                    path.remove((i,j))
-        return False
+            k = i - 1
+            while value_dict.get(k) is not None:
+                temp_length += 1
+                value_dict[k] = True
+                k -= 1
 
-
-
-
+            value_dict[i] = True
+            if temp_length > longest_ans:
+                longest_ans = temp_length
+        return longest_ans
