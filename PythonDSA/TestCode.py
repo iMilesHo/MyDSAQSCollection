9d55e8720522f8e3e3aadee5904af276c12c4430
Lyle <imilesho@gmail.com>
2024-07-08 23:55:09 -0400

    n

--- PythonDSA/TestCode.py
+++ PythonDSA/TestCode.py
@@ -1,28 +1,31 @@
 class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
-        value_dict = {}
-        for i in nums:
-            value_dict[i] = False
-        longest_ans = 0
-        for i in nums:
-            if value_dict[i]:
-                continue
-            
-            temp_length = 1
-            j = i + 1
-            while value_dict.get(j) is not None:
-                temp_length += 1
-                value_dict[j] = True
-                j += 1
-                
-            
-            k = i - 1
-            while value_dict.get(k) is not None:
-                temp_length += 1
-                value_dict[k] = True
-                k -= 1
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        if nums[0] != nums[1]:
+            return nums[0]
+        if nums[-1] != nums[-2]:
+            return nums[-1]
 
-            value_dict[i] = True
-            if temp_length > longest_ans:
-                longest_ans = temp_length
-        return longest_ans
+        left = 0
+        right = len(nums) - 1
+        mid = (left + right) // 2
+        while left <= right:
+            if nums[mid] != nums[mid-1] and nums[mid] != nums[mid+1]:
+                return nums[mid]
+            elif mid % 2 == 0:
+                if nums[mid] == nums[mid+1]:
+                    left = mid + 1
+                    mid = (left + right) // 2
+                else:
+                    right = mid - 1
+                    mid = (left + right) // 2
+            else:
+                if nums[mid] != nums[mid+1]:
+                    left = mid + 1
+                    mid = (left + right) // 2
+                else:
+                    right = mid - 1
+                    mid = (left + right) // 2
+        
+        return nums[mid] 
