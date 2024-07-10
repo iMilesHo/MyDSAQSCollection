4673ec616942ae0add7a4deae7b34f5517bad06e
Lyle <imilesho@gmail.com>
2024-07-09 23:44:17 -0400

    n

--- PythonDSA/TestCode.py
+++ PythonDSA/TestCode.py
@@ -1,31 +1,18 @@
 class Solution:
-    def singleNonDuplicate(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        if nums[0] != nums[1]:
-            return nums[0]
-        if nums[-1] != nums[-2]:
-            return nums[-1]
-
+    def searchInsert(self, nums: List[int], target: int) -> int:
         left = 0
         right = len(nums) - 1
-        mid = (left + right) // 2
+        mid = int((left + right) / 2)
         while left <= right:
-            if nums[mid] != nums[mid-1] and nums[mid] != nums[mid+1]:
-                return nums[mid]
-            elif mid % 2 == 0:
-                if nums[mid] == nums[mid+1]:
-                    left = mid + 1
-                    mid = (left + right) // 2
-                else:
-                    right = mid - 1
-                    mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+                mid = int((left + right) / 2)
             else:
-                if nums[mid] != nums[mid+1]:
-                    left = mid + 1
-                    mid = (left + right) // 2
-                else:
-                    right = mid - 1
-                    mid = (left + right) // 2
-        
-        return nums[mid] 
+                right = mid - 1
+                mid = int((left + right) / 2)
+        if nums[mid] < target:
+            return mid + 1
+        else:
+            return mid
