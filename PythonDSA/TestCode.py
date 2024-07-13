16b1a32f64428d60d6e7003ae21e60a7e4e1541b
Lyle <imilesho@gmail.com>
2024-07-12 23:53:20 -0400

    n

--- PythonDSA/TestCode.py
+++ PythonDSA/TestCode.py
@@ -1,16 +1,44 @@
 class Solution:
-    def isPerfectSquare(self, num: int) -> bool:
-        start = 1
-        end = num
-        mid = (start + end) // 2
-        while start <= end:
-            target = mid**2
-            if target == num:
-                return True
-            elif target < num:
-                start = mid + 1
-                mid = (start + end) // 2
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) <= 3:
+            if target in nums:
+                return nums.index(target)
             else:
-                end = mid - 1
-                mid = (start + end) // 2
-        return False   
+                return -1
+
+        left, right = 0, len(nums) - 1
+        mid = (left + right) // 2
+        while left <= right:
+            if nums[mid] > nums[-1]:
+                left = mid + 1
+                mid = (left + right) // 2
+            else:
+                right = mid - 1
+                mid = (left + right) // 2
+        minimum_index = left
+
+        # 0, minimum_index - 1
+        left, right = 0, minimum_index - 1
+        mid = (left + right) // 2
+        while left <= right:
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+                mid = (left + right) // 2
+            else:
+                right = mid - 1
+                mid = (left + right) // 2
+        # minimum_index, len(nums) - 1
+        left, right = minimum_index, len(nums) - 1
+        mid = (left + right) // 2
+        while left <= right:
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+                mid = (left + right) // 2
+            else:
+                right = mid - 1
+                mid = (left + right) // 2
+        return -1
