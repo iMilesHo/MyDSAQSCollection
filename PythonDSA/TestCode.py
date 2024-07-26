d779bfaf0f3f30cc63a29e254e90cbc6e9788d48
Lyle <imilesho@gmail.com>
2024-07-25 23:43:03 -0400

    n

--- PythonDSA/TestCode.py
+++ PythonDSA/TestCode.py
@@ -1,30 +1,19 @@
-class TimeMap:
-
-    def __init__(self):
-        self.container = {}
-
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.container.keys():
-            self.container[key] = [(value, timestamp)]
-        else:
-            self.container[key].append((value,timestamp))
-
-    def get(self, key: str, timestamp: int) -> str:
-        if key not in self.container.keys():
-            return ""
-        value_timestamp_list = self.container[key]
-        if value_timestamp_list[0][1] > timestamp:
-            return ""
-
-        left, right = 0, len(value_timestamp_list) - 1
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return min(nums)
+            
+        left = 0
+        right = len(nums) - 1
         mid = (left + right) // 2
+
         while left <= right:
-            if value_timestamp_list[mid][1] == timestamp:
-                return value_timestamp_list[mid][0]
-            elif value_timestamp_list[mid][1] < timestamp:
+            if nums[mid] > nums[-1]:
                 left = mid + 1
                 mid = (left + right) // 2
             else:
                 right = mid - 1
                 mid = (left + right) // 2
-        return value_timestamp_list[right][0]   
+        return nums[left]   
