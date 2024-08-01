d8b35848a774f4f519bd0ce380a8ccb064d407c1
Lyle <imilesho@gmail.com>
2024-07-31 23:49:27 -0400

    n

--- PythonDSA/TestCode.py
+++ PythonDSA/TestCode.py
@@ -1,47 +1,18 @@
 class Solution:
-    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if (not head and not head.next) or k == 1:
-            return head
-
-        # get the length of the linked list
-        length = 0
-        temp = head
-        while temp:
-            length += 1
-            temp = temp.next
-
-        pre = None
-        mid = head
-
-        newHead = None
-        group_trail = head
-        next_group_trail = None
-
-        count = 0
-
-        last_group_count = length//k * k
-
-        while mid and count < last_group_count:
-            after = mid.next
-            mid.next = pre
-            pre = mid
-            mid = after
-
-            count += 1
-            if count == k:
-                newHead = pre
-            if count % k == 0:
-                if group_trail == head:
-                    head = pre
-                    next_group_trail = mid
-                else:
-                    group_trail.next = pre
-                    group_trail = next_group_trail
-                    group_trail.next = None
-                    pre = None
-                    next_group_trail = mid
-
-        # if the last group is less than k
-        group_trail.next = mid
-
-        return newHead
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+        
+        for r in range(9):
+            for c in range (9):
+                if board[r][c] == ".":
+                    continue
+                if (board [r][c] in rows [r] or
+                    board [r][c] in cols [c] or
+                    board [r][c] in squares [(r // 3, c //3)]):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares [(r//3, c//3)].add(board[r][c])
+        return True
