e870e2d8970dfd4b843d8f54b9d1611320ccd4e2
Lyle <imilesho@gmail.com>
2024-08-04 00:05:55 -0400

    n

--- PythonDSA/TestCode.py
+++ PythonDSA/TestCode.py
@@ -1,30 +1,22 @@
 class Solution:
-    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        def reverseList(head, length, k):
-            if length < k:
-                return head
-            pre = None
-            mid = head
-            after = None
-            count = 0
-            while mid and count < k:
-                after = mid.next
-                mid.next = pre
-                pre = mid
-                mid = after
-                count += 1
-            if mid:
-                head.next = reverseList(mid, length - k, k)
-            return pre
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        if grid[0][0] == 1:
+            return -1
+        from collections import deque
+        Rows = len(grid) # 3
+        Columns = len(grid[0]) # 4
+        sr,sc,tr,tc = 0, 0, Rows - 1, Columns - 1
+        bfs_queue = deque()
+        bfs_queue.append((sr,sc,1)) # [(0,0,1)]
+        seen = set()
+        seen.add((sr,sc)) # {(0, 0)}
 
-        if (not head and not head.next) or k == 1:
-            return head
-
-        # get the length of the linked list
-        length = 0
-        temp = head
-        while temp:
-            length += 1
-            temp = temp.next
-
-        return reverseList(head, length, k)
+        while len(bfs_queue) > 0:
+            r, c, depth = bfs_queue.popleft()
+            if r == tr and c == tc:
+                return depth
+            for (i_r, i_c) in [(r+1, c), (r-1, c), (r, c+1), (r, c-1), (r+1,c+1), (r-1,c+1), (r+1,c-1),(r-1,c-1)]: # [(1, 0), (-1, 0), (0, 1), (0, -1)]
+                if 0 <= i_r < Rows and 0 <= i_c < Columns and grid[i_r][i_c] == 0 and (i_r, i_c) not in seen:
+                    bfs_queue.append((i_r, i_c, depth+1))
+                    seen.add((i_r, i_c))
+        return -1
