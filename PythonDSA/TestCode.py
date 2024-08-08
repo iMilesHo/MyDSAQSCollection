3d1069edb4964fd38ab107e02c11e4e6ea22a98a
Lyle <imilesho@gmail.com>
2024-08-07 23:42:24 -0400

    n

--- PythonDSA/TestCode.py
+++ PythonDSA/TestCode.py
@@ -1,46 +1,18 @@
-  from collections import defaultdict
-
-  # Initialize the character count map
-  char_count = defaultdict(int)
-  required_chars = len(arr)
-  left, right = 0, 0
-  min_length = float('inf')
-  min_substr = ""
-  unique_chars = 0
-
-
-  # Populate the character count map with the characters in arr
-  for char in arr:
-    char_count[char] = 0
-
-  while right < len(str):
-
-    # Expand the window by including the character at right index
-    if str[right] in char_count:
-      if char_count[str[right]] == 0:
-        unique_chars += 1
-      char_count[str[right]] += 1
+def HackerRank(s):
+  if not s or s == "":
+    return 0
+  stack = []
+  for c in s:
+    if len(stack) > 0 and ((c == '0' and stack[-1] == '1') or (c == '1' and stack[-1] == '0')):
+      stack.pop()
+    else:
+      stack.append(c)
+  return len(stack)
 
 
-
-    while unique_chars == required_chars:
-      if right - left + 1 < min_length:
-        min_length = right - left + 1
-        min_substr = str[left:right+1]
-        # update the min_subst
-      if str[left] in char_count:
-        char_count[str[left]] -= 1
-        if char_count[str[left]] == 0:
-          unique_chars -= 1
-      left += 1
-
-    right += 1
-    # check if the new substing is minimum
-
-  return min_substr
-
-
-# Example usage:
-arr = ['x', 'y', 'z']
-str = "xyyzyzyx"
-print(get_shortest_unique_substring(arr, str))  # Output: "zyx"
+# test cases
+print(HackerRank("1100")) # 0
+print(HackerRank("1111")) # 4
+print(HackerRank("01010")) # 1
+print(HackerRank("010101")) # 0
+print(HackerRank("111&000")) # 0
