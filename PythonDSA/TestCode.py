d90051d6fb39ae30c1baab65aaa799781f6f2d49
Lyle <imilesho@gmail.com>
2024-08-08 23:48:41 -0400

    n

--- PythonDSA/TestCode.py
+++ PythonDSA/TestCode.py
@@ -1,18 +1,17 @@
-def HackerRank(s):
-  if not s or s == "":
-    return 0
-  stack = []
-  for c in s:
-    if len(stack) > 0 and ((c == '0' and stack[-1] == '1') or (c == '1' and stack[-1] == '0')):
-      stack.pop()
-    else:
-      stack.append(c)
-  return len(stack)
-
-
-# test cases
-print(HackerRank("1100")) # 0
-print(HackerRank("1111")) # 4
-print(HackerRank("01010")) # 1
-print(HackerRank("010101")) # 0
-print(HackerRank("111&000")) # 0
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        mid = int((left + right) / 2)
+        while left <= right:
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                # target is in the right side of index mid
+                left = mid + 1
+                mid = int((left + right) / 2)
+            else:
+                right = mid - 1
+                mid = int((left + right) / 2)
+            
+        return -1     
