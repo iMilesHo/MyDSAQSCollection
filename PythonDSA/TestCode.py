1eb1c88be20791e7ebd9cb2f15f5e4d436c4d535
Lyle <imilesho@gmail.com>
2024-08-09 23:42:04 -0400

    n

--- PythonDSA/TestCode.py
+++ PythonDSA/TestCode.py
@@ -1,17 +1,15 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def guessNumber(self, n: int) -> int:
         left = 0
-        right = len(nums) - 1
+        right = n
         mid = int((left + right) / 2)
         while left <= right:
-            if nums[mid] == target:
+            if guess(mid) == 0:
                 return mid
-            elif nums[mid] < target:
-                # target is in the right side of index mid
+            elif guess(mid) == 1:
                 left = mid + 1
                 mid = int((left + right) / 2)
             else:
                 right = mid - 1
                 mid = int((left + right) / 2)
-            
-        return -1     
+        return None
